cfdc4259d736a2c8a43a4cb5488ed48ada29c06c
EdwinJose <edwinjose900@gmail.com>
2017-06-16 18:27:39 +0530

    error solved

--- des.py
+++ des.py
@@ -4,10 +4,8 @@ import numpy as np
 import os
 
 def create_pos_n_neg():
-    for file_type in ['neg']:
-        
+    for file_type in ['neg']:       
         for img in os.listdir(file_type):
-
             if file_type == 'pos':
                 line = file_type+'/'+img+' 1 0 0 50 50\n'
                 with open('info.dat','a') as f:
@@ -17,5 +15,5 @@ def create_pos_n_neg():
                 with open('bg.txt','a') as f:
                     f.write(line)
 
+
 create_pos_n_neg()
-# this file is having some complilation errors just look into it and let me know asap
